66e65a2aacf65c217d734b4003d527c16fce2f46
Abinash Dhakal <abu.dhakal1@gmail.com>
2024-09-22 16:21:00 +0100

    Purchase_Recency

--- purchase_recency.sql
+++ purchase_recency.sql
@@ -1,10 +1,9 @@
- -- Customer Segmentation
 WITH customer_stats AS (
     SELECT
         customer_id,
         SUM(quantity * list_price * (1 - discount)) AS total_spent,
         COUNT(DISTINCT orders.order_id) AS total_orders,
-        DATEDIFF(DAY, MAX(orders.order_date), '2018-12-29')  AS days_since_last_purchase
+        DATEDIFF(DAY, MAX(orders.order_date), '2018-12-29') AS days_since_last_purchase
     FROM
         [Bike_Store_Relation].[dbo].[orders]
     INNER JOIN
@@ -14,18 +13,14 @@ WITH customer_stats AS (
     GROUP BY
         customer_id
 )
-
 SELECT
-    customer_id,
-    CASE WHEN total_orders > 1 THEN 'repeat buyer'
-         ELSE 'one-time buyer'
-         END AS purchase_frequency,
     CASE WHEN days_since_last_purchase < 90 THEN 'recent buyer'
          ELSE 'not recent buyer'
          END AS purchase_recency,
-    CASE WHEN total_spent/(SELECT MAX(total_spent) FROM customer_stats) >= .65 THEN 'big spender'
-         WHEN total_spent/(SELECT MAX(total_spent) FROM customer_stats) <= .30 THEN 'low spender'
-         ELSE 'average spender' 
-         END AS buying_power
+    COUNT(*) AS Total
 FROM
-    customer_stats;
+    customer_stats
+GROUP BY
+    CASE WHEN days_since_last_purchase < 90 THEN 'recent buyer'
+         ELSE 'not recent buyer'
+         END;
