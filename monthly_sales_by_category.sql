1181791104e12f70e716f341419645fbfdfdcf15
Abinash Dhakal <abu.dhakal1@gmail.com>
2024-09-23 18:43:16 +0100

    Monthly_sales_by_Category

---
+++ monthly_sales_by_category.sql
@@ -0,0 +1,40 @@
+WITH product_categories AS (
+    SELECT
+        product_id,
+        category_name
+    FROM
+        [Bike_Store_Relation].[dbo].[products]
+    INNER JOIN
+        [Bike_Store_Relation].[dbo].[categories]
+    ON
+        products.category_id = categories.category_id
+),
+
+product_sales_ym AS (
+    SELECT
+        YEAR(order_date) AS year,
+        MONTH(order_date) AS month,
+        product_id,
+        SUM(quantity) AS units_sold
+    FROM
+        [Bike_Store_Relation].[dbo].[orders]
+    INNER JOIN
+        [Bike_Store_Relation].[dbo].[order_items]
+    ON
+        orders.order_id = order_items.order_id
+    GROUP BY
+        YEAR(order_date), MONTH(order_date), product_id
+)
+
+SELECT
+    month,
+    category_name,
+    AVG(units_sold) AS avg_units_sold
+FROM
+    product_sales_ym
+INNER JOIN
+    product_categories
+ON
+    product_sales_ym.product_id = product_categories.product_id
+GROUP BY
+    month, category_name;
